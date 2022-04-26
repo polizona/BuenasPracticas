5af1ef2bd9e0683bdf12dce2bb824a9baa3203f3
profegil <profe.gilberto.ramirez@gmail.com>
2022-04-26 16:43:43 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+	
+<body>
+<div class="container">
+<div class="row align-items-center">
+<div class="col-12 align-self-center text-center p-4">
+<h1>Escribe tu nombre</h1>
+<h2> Extraordinario de Dise�o de Sistemas </h2>
+<h2> 2CM32 </h2>								
+<br>
+
+<form action="recupera.php" method="get">
+<button type="submit">consulta</button>
+</form>
+<br>
+					
+
+<form action="graba.php" method="post">
+<fieldset>
+  <legend> Ingrese alcaldia </legend>
+    <p><label>idalcaldia:<input type="int" name="idalcaldia" /></label> </p>
+    <p><label>nombre:   <input type="text" name="alcaldia" /></label></p>
+    <p><input type="submit" value="enviar"/></p>
+</fieldset>
+</form>
+</div>
+</div>	
+</div>
+</body>
+</html>
