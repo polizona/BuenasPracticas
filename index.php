e09970d69c83ebfaab4a655fb7d0e08fed15f6cf
profegil <profe.gilberto.ramirez@gmail.com>
2022-06-16 17:35:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <html>
+	
 <body>
 <div class="container">
 <div class="row align-items-center">
 <div class="col-12 align-self-center text-center p-4">
-<h1>Escribe tu nombre</h1>
-<h2> Buenas prácticas de software</h2>
-<h2> Objetivo de aprendizaje </h2>								
+<h1>escribe tu nombre</h1>
+Crea una tabla llamada alcaldia con los campos idalcaldia(int) y nbalcaldia(text)
 <br>
-<p> Integrar los componentes de un sistema inteligente con base en estándares tecnológicos industriales para intercambiar objetos entre el navegador y los servidores de procesos, datos e inteligencia. </p>
+Edita los archivos graba.php  y recupera.php con tu usuario y contrase�a de la base de datos
+<br> <br>
+
+<form action="recupera.php" method="get">
+<button type="submit">consulta</button>
+</form>
+<br>
+					
+
+<form action="graba.php" method="post">
+<fieldset>
+  <legend> Ingrese alcaldia </legend>
+    <p><label>idalcaldia:<input type="int" name="idalcaldia" /></label> </p>
+    <p><label>nombre:   <input type="text" name="alcaldia" /></label></p>
+    <p><input type="submit" value="enviar"/></p>
+</fieldset>
+</form>
 </div>
 </div>	
 </div>
